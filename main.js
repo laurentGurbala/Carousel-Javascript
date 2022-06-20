935f932307aeacfa5fce763b852e3c045aab7546
Gurbala Laurent <69318652+laurentGurbala@users.noreply.github.com>
2022-06-20 16:38:19 +0200

    Création des items dans le markup

--- main.js
+++ main.js
@@ -1,23 +1,24 @@
 const carousel = document.querySelector(".carousel");
 const container = document.querySelector(".container");
+const children = [].slice.call(container.children);
 const btnLeft = document.querySelector(".btn--left");
 const btnRight = document.querySelector(".btn--right");
 
-let nbItem = 5;
+let nbItem = children.length;
 let currentIndex = 0;
 
 // Agrandit la largeur du container
 container.style.width = `${carousel.offsetWidth * nbItem}px`;
 
 // Crée les items du carousel
-for (let i = 1; i <= nbItem; i++) {
-    const item = nbItem[i];
-    
-    let div = document.createElement("div");
-    div.className = "item__img";
-    div.style.backgroundImage = `url('image/image_${i}.jpg')`;
-    container.appendChild(div);
-}
+// for (let i = 1; i <= nbItem; i++) {
+//     const item = nbItem[i];
+
+//     let div = document.createElement("div");
+//     div.className = "item__img";
+//     div.style.backgroundImage = `url('image/image_${i}.jpg')`;
+//     container.appendChild(div);
+// }
 
 // Ajout des évent aux boutons
 btnLeft.addEventListener("click", BtnLeftClick);
@@ -44,13 +45,13 @@ function BtnRightClick() {
 }
 
 function ToogleShowBtn() {
-    if (currentIndex <= 0) 
-        btnLeft.style.visibility = "hidden";
+    if (currentIndex <= 0)
+        btnLeft.classList.add("hidden");
     else
-        btnLeft.style.visibility = "visible";
+        btnLeft.classList.remove("hidden");
 
     if (currentIndex >= nbItem - 1)
-        btnRight.style.visibility = "hidden";
+        btnRight.classList.add("hidden");
     else
-        btnRight.style.visibility = "visible";
+        btnRight.classList.remove("hidden");
 }
